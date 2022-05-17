4d003bcec1e1ba302ebbea41269555e202e92874
1MrHad1 <umaildanish776@gmail.com>
2022-05-17 16:40:04 +0530

    Genrator

---
+++ HGen.js
@@ -0,0 +1,109 @@
+let h1 = document.querySelector("h1");
+let button = document.querySelector("button");
+button.onclick = function () {
+    let HadeesNum = Math.floor(Math.random() * 32)
+    switch (HadeesNum) {
+        case 0:
+            h1.innerHTML = ' Hadhrat Jaabir (Allah be pleased with him) narrates that Muhammad (peace be upon him) said: "Salaat is the key to Jannat"'
+        break;
+        case 1:
+            h1.innerHTML = 'There are three signs of a hypocrite: when he speaks he speaks lies, when he makes a promise he breaks it, and when he is trusted he betrays his trust (Bukhari)'
+        break;
+        case 2:
+            h1.innerHTML = ' When a man dies no further reward is recorded for his actions, with three exceptions: Sadqa which continues to be supplied, or knowledge from which benefit continues to be reaped, or the prayers of a good son to his dead father (Muslim)'
+        break;
+        case 3:
+            h1.innerHTML = ' What is Faith? When your good deed pleases you and your evil deed grieves you, you are a believer. What is Sin? When a thing disturbs (the peace of) your heart, give it up.'
+        break;
+        case 4:
+            h1.innerHTML = ' Nothing but supplication averts the decree, and nothing but righteousness increases life'
+        break;
+        case 5:
+            h1.innerHTML = ' He who remembers his Lord and he who does not are like the living and dead (Bukhari, Muslim)'
+        break;
+        case 6:
+            h1.innerHTML = ' If you spend (to help others), O son of Adam! [ALLHA] shall spend on you.(Bukhari, Muslim)'
+        break;
+        case 7:
+            h1.innerHTML = ' Do not consider any act of kindness insignificant, even meeting your brother with a cheerful face.'
+        break;
+        case 8:
+            h1.innerHTML = ' Among the believers who show most perfect faith are those who have best disposition and are kindest to their families (Tirmidhi)'
+        break;
+        case 9:
+            h1.innerHTML = 'When a woman observes the five time of prayers, fasts during Ramadan, preserves her chastity, and obeys her husband, she may enter by any of the gates of paradise she wishes.'
+        break;
+        case 10:
+            h1.innerHTML = ' When any of you eats, he should eat with his right hand and when he drinks he should drink with his right hand'
+        break;
+        case 11:
+            h1.innerHTML = ' When you visit an invalid tell him to make a supplication for you, for his supplication is like of the angels.'
+        break;
+        case 12:
+            h1.innerHTML = ' A Muslim has five duties towards another Muslim; to return a salutation, visit the sick, follow funerals, accept an invitation and say (God have mercy on you) when one sneezes.'
+        break;
+        case 13:
+            h1.innerHTML = ' What rights can parents demand from their children?" He (peace and blessings of Allah be upon him) replied, "They are your paradise and your hell'
+        break;
+        case 14:
+            h1.innerHTML = ' By Him in Whose hand my soul is, a man does not believe till he likes for his brother what he likes for himself (Bukhari, Muslim)'
+        break;
+        case 15:
+            h1.innerHTML = ' All creatures are Gods children, and those dearest to God are the ones who treat His children kindly'
+        break;
+        case 16:
+            h1.innerHTML = ' The believer is not the one who eats his fill when the neighbour beside him is hungry.'
+        break;
+        case 17:
+            h1.innerHTML = ' He does not belong to us who does not show mercy to our young ones and respect to our old ones, who does not recommend what is reputable and prohibit what is disreputable.'
+        break;
+        case 18:
+            h1.innerHTML = ' If anyone guarantees me what is between his jaws and what is between his legs (i.e. he should not commit fornication), I shall guarantee him paradise.(Bukhari)'
+        break;
+        case 19:
+            h1.innerHTML = ' Reviling a Muslim is disobedience to God, and fighting with him is infidelity..'
+        break;
+        case 20:
+            h1.innerHTML = ' If you guarantee me six things on your part, I shall guarantee you paradise: speak the truth when you talk, keep a promise when you make it, when you are trusted with something fulfil your trust, avoid sexual immorality, lower your eyes and restrain your hand from injustice.(Baihaqi)'
+        break;
+        case 21:
+            h1.innerHTML = ' God has revealed to me that you must be humble, so that no one boasts over another, or oppresses another.(Muslim)'
+        break;
+        case 22:
+            h1.innerHTML = ' he one who is most honourable in Allha`s estimation is the pious.'
+        break;
+        case 23:
+            h1.innerHTML = ' It is not allowable for a man to keep apart from his brother for more than three days, the one turning away and the one turning away when they meet. The better of the two is the one who is first to give a greeting.(Bukhari, Muslim).'
+        break;
+        case 24:
+            h1.innerHTML = ' Avoid envy, for envy devours good deeds just as fire devours fuel.(Abu Dawud R.A).'
+        break;
+        case 25:
+            h1.innerHTML = 'Righteousness is good character, and sin is that which revolves in your heart and which you do not want people to know.'
+        break;
+        case 26:
+            h1.innerHTML = 'Modesty is part of faith and faith is in paradise, but obscenity is part of hardness of heart and hardness of heart is in hell (Ahmad, Tirmidhi).'
+        break;
+        case 27:
+            h1.innerHTML = ' The believers whose faith is most perfect are those who have the best character'
+        break;
+        case 28:
+            h1.innerHTML = 'God will not show mercy to him who does not show mercy to others.'
+        break;
+        case 29:
+            h1.innerHTML = 'If anyone cares for two girls till they grow up, he and I will come on the Day of Resurrection thus," and he joined fingers..(Muslim).'
+        break;
+        case 30:
+            h1.innerHTML = 'A believer is not stung twice from the same hole.'
+        break;
+        case 31:
+            h1.innerHTML = 'When one of you looks at someone who is superior to him in property and appearance, he should look at someone who is inferior to him..'
+        break;
+        case 32:
+            h1.innerHTML = 'Seek me among you weak ones, for you are given provision from God, God will be satisfied with few good deeds from him.(Baihaqi)..'
+        break;
+        
+
+        
+    }
+}
